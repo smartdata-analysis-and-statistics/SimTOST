3c78c3ad72cf6ffa085721a047f2fb2e6d1a4a72
NightlordTW <tdebray@fromdatatowisdom.com>
2025-01-10 13:59:34 +0100

    fix calculation of no. of endpoints

--- R/helper.r
+++ R/helper.r
@@ -21,12 +21,12 @@ print.simss <- function(x, ...) {
   output <- data.frame(Parameter = c("Total Sample Size", "Achieved Power", "Power Confidence Interval"),
                        Value = c(sst, 100*power, paste0(100*lpower, " - ", 100*upower)))
 
-  message("Sample Size Calculation Results")
+  cat("Sample Size Calculation Results\n")
   cat("-------------------------------------------------------------\n")
   cat(paste0("Study Design: ", x$param.d$dtype, " trial targeting ",100*tpower,"% power with a ",100*alpha, "% type-I error.\n"))
   cat("Endpoints Tested:\n")
   for (i in 1:length(x$param$list_comparator)) {
-    nendp <- length(x$param$list_comparator[[i]])
+    nendp <- length(x$param$list_y_comparator[[i]])
     str <- paste0("  - ", paste(x$param$list_comparator[[i]], collapse = " vs. "), ": ",
                   paste(x$param$list_y_comparator[[i]], collapse = ", "))
     if (nendp > 1 & x$param.d$k[i] < nendp) {
